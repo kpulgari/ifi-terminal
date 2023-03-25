633d11273b8065344fab9126042212036c0969fc
Kaushik Pulgari <kpulg2@illinois.edu>
2023-03-25 18:59:00 -0500

    fixing gitignore

---
+++ src/utils/secrets.py
@@ -0,0 +1,8 @@
+# Gitignored file for storing secrets like API keys and passwords
+
+FINNHUB_API_KEY = 'cgdq8g9r01qpf4i29llgcgdq8g9r01qpf4i29lm0'
+
+REDDIT_API_TOKEN = 'odzTgvfSnc3y31VaprfBfw6Vx3r1bQ'
+REDDIT_API_CLIENT_ID = 'l4wekIgazDlIjZxI8Y1zdQ'
+REDDIT_USERNAME = 'Perfect_Ad_5981'
+REDDIT_PASSWORD = 'Password@123'
