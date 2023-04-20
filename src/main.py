d5cd51e51a14643324530688ad9d34cf93bcf03a
Suchit Bapatla <Suchbap@gmail.com>
2023-04-19 19:59:28 -0500

    added updating display

--- src/main.py
+++ src/main.py
@@ -6,11 +6,14 @@ from rich.console import Console
 from rich.table import Table
 from rich.color import Color
 from rich.style import Style
+from rich.live import Live
+from rich.table import Table
 
 from utils.secrets import REDDIT_API_TOKEN, REDDIT_API_CLIENT_ID, REDDIT_USERNAME, REDDIT_PASSWORD
 from utils.secrets import FINNHUB_API_KEY as API_KEY
 
 import random
+import time
 
 
 if __name__ == "__main__":
@@ -30,8 +33,6 @@ if __name__ == "__main__":
         user_choice_selection = input(f"{choices}Please enter a comma-separated list to select filters: ").replace(" ", "")
         selection_arr = user_choice_selection.split(",")
 
-        table = Table(title="YFinance Testing")
-
         colorMapping = {
             "dayHigh": "green",
             "dayLow": "red",
@@ -39,20 +40,6 @@ if __name__ == "__main__":
             "fiftyDayAverage": "purple"
         }
 
-        table.add_column("STOCK", style="bold cyan")
-
-        column_cache = []
-
-        for selection in selection_arr:
-            column = fast_info_choices[int(selection) - 1]
-
-            if column in column_cache:
-                pass
-            else:
-                style = Style(color=Color.from_rgb(random.randint(100, 255), random.randint(100, 255), random.randint(100, 255)))
-                style = colorMapping.get(column, style)
-                table.add_column(column.upper(), style=style, justify="center")  
-                column_cache.append(column) 
 
         stock_cache = []
 
@@ -66,18 +53,41 @@ if __name__ == "__main__":
                     if stock in stock_cache:
                         raise ValueError()
                     
-                    yfinance_api = YFinanceAPI(stock)
-                    row_values = [stock] + [str(yfinance_api.fast_info[column]) for column in column_cache]
-                    table.add_row(*row_values)
                     stock_cache.append(stock)
                 except:
                     print(f"{stock} has already been added!")
 
             except Exception as e:
                 print("Invalid stock!")
+
+
+
+        def generate_table() -> Table:
+            column_cache = []
+            """Make a new table."""
+            table = Table(title="YFinance Testing")
+            table.add_column("STOCK", style="bold cyan")
+            for selection in selection_arr:
+                column = fast_info_choices[int(selection) - 1]
+                
+
+                style = Style(color=Color.from_rgb(random.randint(100, 255), random.randint(100, 255), random.randint(100, 255)))
+                style = colorMapping.get(column, style)
+                table.add_column(column.upper(), style=style, justify="center")  
+                column_cache.append(column) 
+
+            for stock in stock_cache:
+                yfinance_api = YFinanceAPI(stock)
+                row_values = [stock] + [str(yfinance_api.fast_info[column]) for column in column_cache]
+                table.add_row(*row_values)
+            return table
+
         
-        console = Console()
-        console.print(table)
+        with Live(generate_table(), refresh_per_second=4) as live:
+            for _ in range(5):
+                time.sleep(5)
+                live.update(generate_table())
+                print("updated")
 
     elif selection == "R":
         reddit_api = RedditAPI(REDDIT_API_TOKEN, REDDIT_API_CLIENT_ID, REDDIT_USERNAME, REDDIT_PASSWORD)
